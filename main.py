c853fb31328e357d5dc52e076d898051d29217ce
agharib <92991636+NBCoder123@users.noreply.github.com>
2021-11-02 09:52:40 +0000

    Function added

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
-i = input("Enter your name: ")
-print("Welcome", i)
+
+def myFunction():
+  count = 0
+  a = int(input("Please enter a number between 1 to 50: "))
+  if a >= 20 and a <= 50:
+    for i in range(1,21):
+      print(i)
+  
+
+
+
+  for a in range(1,21):
+    count = count + 1
+    print(count)
+    if count == 5:
+      break
+    
+myFunction()
+ 
+data = [1,2,3]
+for x in data:
+    print(x, end=' ')
+
+
+
