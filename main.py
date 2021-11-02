73636b7b6c4363810af704b8f8550fefe88bcd6e
agharib <92991636+NBCoder123@users.noreply.github.com>
2021-11-02 13:13:47 +0000

    Nothing specific.

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
+L3 = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+L3[0] #prints index four from the list
 
-def myFunction():
-  count = 0
-  a = int(input("Please enter a number between 1 to 50: "))
-  if a >= 20 and a <= 50:
-    for i in range(1,21):
-      print(i)
-  
+print(L3[0], L3[2])
 
-
-
-  for a in range(1,21):
-    count = count + 1
-    print(count)
-    if count == 5:
-      break
-    
-myFunction()
- 
-data = [1,2,3]
-for x in data:
-    print(x, end=' ')
 
 
 
